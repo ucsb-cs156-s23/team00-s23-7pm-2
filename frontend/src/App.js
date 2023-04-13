dd4f8a30e3ceabb902edc80abcc026fd3365c3e2
JNewman-Cell <jackson_newman@ucsb.edu>
2023-04-12 19:39:04 -0700

    cg - added route in App.js for San Francisco

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import HomePage from "main/pages/HomePage";
 import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
+import SanFranciscoPage from "main/pages/SanFranciscoPage";
 
 import "bootstrap/dist/css/bootstrap.css";
 import SanFranciscoPage from "main/pages/SanFranciscoPage";
