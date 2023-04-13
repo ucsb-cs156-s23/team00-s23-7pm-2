4086a1ba614970febc7f4353bc36ce476bd94cd6
Maya Ma <yema@cs.ucsb.edu>
2023-04-12 19:39:17 -0700

    my - added Route in App.js for New York

--- frontend/src/main/pages/NewYorkPage.js
+++ frontend/src/main/pages/NewYorkPage.js
@@ -6,7 +6,8 @@ export default function NewYorkPage() {
       <div className="pt-2">
         <h1>New York, NY</h1>
         <p>
-         New York, New York is often refered to as New York City or NYC. It is the most populus city in the United States. It is sitting where the Hudson River meets the Atlantic Ocean. 
+         New York, New York is often refered to as New York City or NYC. 
+         It is the most populus city in the United States. It is sitting where the Hudson River meets the Atlantic Ocean. 
         </p>
         <p>Added by Maya M.</p>
       </div>
