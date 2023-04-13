1774bd7da9ce9258f8c98efce70fe8c7bb8051a6
Colin Baylis <colinbaylis@ucsb.edu>
2023-04-13 10:11:28 -0700

    CB - added route in App.js for Santa Cruz

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import AvilaBeachPage from "main/pages/AvilaBeachPage";
 import LosAlamosPage from "main/pages/LosAlamosPage";
 import ArroyoGrandePage from "main/pages/ArroyoGrandePage";
 import LosAngeles from "main/pages/LosAngeles";
+import SantaCruzPage from "main/pages/SantaCruzPage";
+
 
 import "bootstrap/dist/css/bootstrap.css";
 
@@ -18,6 +20,7 @@ function App() {
         <Route exact path="/towns/LosAlamos" element={<LosAlamosPage />} />
         <Route exact path="/towns/ArroyoGrande" element={<ArroyoGrandePage />} />
         <Route exact path="/towns/LosAngeles" element={<LosAngeles />} />
+        <Route exact path="/towns/SantaCruz" element={<SantaCruzPage />} />
       </Routes>
     </BrowserRouter>
   );
